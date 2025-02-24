700c6b90c9ef128fbe197e51bb92e3c998e240fa
l-halvarsson <linnhalvarsson1997@gmail.com>
2025-02-24 15:50:06 +0100

    Hej
    Hej

---
+++ server/models/post.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("post", { 
+    id:{ 
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true
+    }
+    }); 
+    }; 
+    
