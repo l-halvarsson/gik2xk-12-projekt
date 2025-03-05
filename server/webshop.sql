251b6d03c9afa42569e8eb4909b62c5207e4582a
l-halvarsson <linnhalvarsson1997@gmail.com>
2025-03-05 12:00:40 +0100

    summering
    Skapat tabeller, routes. Tabellerna är dock inte klara.

--- server/webshop.sql
+++ server/webshop.sql
@@ -1,1 +1,6 @@
+drop DATABASE webshop;
 create database webshop;    
+/* kör denna och börja om från början med databasen, 
+det som finns i den kan försvinna. Högerklicka local host 
+new query. Sen högerklocka i nya filen och kopiera från denna fil och högerklicka och
+kör run my sql query.
