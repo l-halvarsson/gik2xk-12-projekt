364fbdf42cac4594756fb73b23295c1c8bfff891
Linnéa Andersson <linneaandersson93@hotmail.com>
2025-03-20 14:07:17 +0100

    router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AllProducts from './views/AllProducts.jsx';
+import CartPage from './views/CartPage.jsx';
+import Home from './views/Home.jsx';
+import ProductDetails from './views/ProductDetails.jsx';
+import ProductEdit from './views/ProductEdit.jsx';
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
